767f5f109f89aa6f94db6c898409547b7fdbdba2
zensan <sanjos100@gmail.com>
2016-09-19 18:28:16 +0530

    Use multi-threading in rate limiter benchmark

--- guava-tests/benchmark/com/google/common/util/concurrent/RateLimiterBenchmark.java
+++ guava-tests/benchmark/com/google/common/util/concurrent/RateLimiterBenchmark.java
@@ -2,6 +2,8 @@ package com.google.common.util.concurrent;
 
 import com.google.common.base.Stopwatch;
 import com.google.common.util.concurrent.RateLimiter.SleepingStopwatch;
+import java.util.List;
+import java.util.ArrayList;
 
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
 
@@ -10,17 +12,47 @@ import static java.util.concurrent.TimeUnit.MICROSECONDS;
  */
 public final class RateLimiterBenchmark {
 
+    public static class LimitRunnable implements Runnable {
+
+        private RateLimiter limiter;
+
+        public LimitRunnable(RateLimiter inputLimiter) {
+           limiter = inputLimiter;
+        }
+        public void run() {
+            long startTime = System.currentTimeMillis();
+
+
+            for (int i = 0; i < 100; i++) {
+                limiter.acquire();
+            }
+            long endTime = System.currentTimeMillis();
+
+            System.out.println(endTime - startTime);
+        }
+    }
+
     public static void main(String[] args) {
 
         RateLimiter.SleepingStopwatch stopwatch = RateLimiter.SleepingStopwatch.createFromSystemTimer();
 
         RateLimiter limiter = RateLimiter.create(stopwatch, 100.0);
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 100; i++) {
-            limiter.acquire();
-        }
-        long endTime = System.currentTimeMillis();
 
-        System.out.println(endTime - startTime);
+        List<Thread> listThreads = new ArrayList<Thread>();
+        for (int i = 0; i < 5; i++) {
+            Runnable r = new LimitRunnable(limiter);
+            Thread t = new Thread(r);
+            t.start();
+            listThreads.add(t);
+        }
+
+        for (Thread t : listThreads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
