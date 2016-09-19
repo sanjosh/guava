728de432b6b2a813b7c01c35b9dab4591361474a
zensan <sanjos100@gmail.com>
2016-09-19 20:32:01 +0530

    check out slack and peak burst (warmup period) in rate limiter

--- guava-tests/benchmark/com/google/common/util/concurrent/RateLimiterBenchmark.java
+++ guava-tests/benchmark/com/google/common/util/concurrent/RateLimiterBenchmark.java
@@ -4,6 +4,7 @@ import com.google.common.base.Stopwatch;
 import com.google.common.util.concurrent.RateLimiter.SleepingStopwatch;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
 
@@ -15,20 +16,36 @@ public final class RateLimiterBenchmark {
     public static class LimitRunnable implements Runnable {
 
         private RateLimiter limiter;
+        private int index;
 
-        public LimitRunnable(RateLimiter inputLimiter) {
-           limiter = inputLimiter;
+        public LimitRunnable(int i, RateLimiter inputLimiter) {
+           index = i; limiter = inputLimiter;
         }
         public void run() {
             long startTime = System.currentTimeMillis();
-
-
             for (int i = 0; i < 100; i++) {
-                limiter.acquire();
-            }
-            long endTime = System.currentTimeMillis();
+                int permit = ThreadLocalRandom.current().nextInt(1, 1024);
 
-            System.out.println(endTime - startTime);
+
+                if (i > 40 & i < 60) {
+                    // create some slack to display peak bursts
+                    try {
+                        Thread.sleep(10); // 10 millisec
+                        permit = 1;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (i > 60 & i < 70) {
+                    // create a peak burst
+                    permit = permit * 4;
+                }
+
+                limiter.acquire(permit);
+                long endTime = System.currentTimeMillis();
+                System.out.printf("%d,%d,%d\n", index, permit, endTime - startTime);
+            }
+
         }
     }
 
@@ -36,11 +53,11 @@ public final class RateLimiterBenchmark {
 
         RateLimiter.SleepingStopwatch stopwatch = RateLimiter.SleepingStopwatch.createFromSystemTimer();
 
-        RateLimiter limiter = RateLimiter.create(stopwatch, 100.0);
+        RateLimiter limiter = RateLimiter.create(stopwatch, 1024.0);
 
         List<Thread> listThreads = new ArrayList<Thread>();
         for (int i = 0; i < 5; i++) {
-            Runnable r = new LimitRunnable(limiter);
+            Runnable r = new LimitRunnable(i, limiter);
             Thread t = new Thread(r);
             t.start();
             listThreads.add(t);
